197772eaae21e9e90d7411587b88cc0209642ac0
MrLuke1618 <byebye1618@gmail.com>
2025-11-03 19:14:19 +0700

    up

---
+++ src/vite-env.d.ts
@@ -0,0 +1,2 @@
+// FIX: Replaced `types` reference with a relative `path` to resolve Vite client types. This is a workaround for a project configuration issue.
+/// <reference path="../node_modules/vite/client.d.ts" />
